9754a98bbbd2e7f5b79fae39d83687399ffa4cb6
ealush <code@ealush.com>
2020-05-19 21:41:26 +0300

    use hrtime

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const GROUP_SIZE = 100;
 const DEFAULT_COUNT = 1000;
+const NS_P_S = 1e9;
+
+const { hrtime } = process;
+
+const toNs = (prev) => {
+  const [s, ns] = hrtime(prev);
+  return s * NS_P_S + ns;
+};
 
 /**
  * easyBench
@@ -16,76 +24,86 @@ const DEFAULT_COUNT = 1000;
 
 // we want to get a significant function run time
 // so we're batching them 100 at a time
-const calcIndex = (counter, fns) => Math.floor((counter / GROUP_SIZE) % fns.length);
+const calcIndex = (counter, fns) =>
+  Math.floor((counter / GROUP_SIZE) % fns.length);
 
 /**
  * Easy to use benchmark function
  * @param description Description for benchmark suite
  */
 const easyBench = (description) => {
-    const fns = [];
-    const api = {};
+  const fns = [];
+  const api = {};
 
-    let startTime;
-    let currentIndex;
+  let startTime;
+  let currentIndex;
 
-    api.add = (name, fn) => {
-        fns.push({name, fn});
-        return api;
-    };
-
-    api.run = (count = DEFAULT_COUNT) => {
-        const fullCount = count * fns.length;
-        let counter = 0;
-
-        while (counter < fullCount) {
-            counter++;
-            let index = calcIndex(counter, fns);
-
-            if (typeof currentIndex === 'undefined') {
-                currentIndex = index;
-                fns[currentIndex].time = fns[currentIndex].time || 0;
-                startTime = Date.now();
-            }
-
-            // handles change of index
-            if (index !== currentIndex) {
-                fns[currentIndex].time += Date.now() - startTime;
-                currentIndex = undefined;
-            }
-
-            // handles last fn edge case
-            if (counter === fullCount) {
-                startTime = Date.now();
-                runner(index);
-                fns[index].time += Date.now() - startTime;
-            } else {
-                runner(index);
-            }
-        }
-        return result();
-    }
-
-    const result = () => {
-        const res = fns.map(({ name, time }) => ({
-            name,
-            time: time + 'ms'
-        }));
-        console.log(`Benchmark for ${description}:`);
-        console.table(res)
-        return res;
-    }
-
-    const runner = (index) => {
-        try {
-            fns[index].fn();
-        } catch (err) {
-            console.error(`Error running ${fns[index].name}:
-                ${err}`)
-        }
-    }
-
+  api.add = (name, fn) => {
+    fns.push({ name, fn });
     return api;
+  };
+
+  api.run = (count = DEFAULT_COUNT) => {
+    const fullCount = count * fns.length;
+    let counter = 0;
+
+    while (counter < fullCount) {
+      counter++;
+      let index = calcIndex(counter, fns);
+
+      if (fns.length === 1) {
+        fns[index].time = fns[index].time || 0;
+        const startTime = hrtime();
+        runner(index);
+        fns[index].time += toNs(startTime);
+        continue;
+      }
+
+      if (typeof currentIndex === "undefined") {
+        currentIndex = index;
+        fns[currentIndex].time = fns[currentIndex].time || 0;
+        startTime = hrtime();
+      }
+      // handles change of index
+      if (index !== currentIndex) {
+        fns[currentIndex].time += toNs(startTime);
+        currentIndex = undefined;
+      }
+
+      // handles last fn edge case
+      if (counter === fullCount) {
+        startTime = hrtime();
+        runner(index);
+        fns[index].time += toNs(startTime);
+      } else {
+        runner(index);
+      }
+    }
+    return result();
+  };
+
+  const result = () => {
+    const res = fns.map(({ name, time }) => ({
+      name,
+      time_ns: time + "ns",
+      time_ms: time / 1e6 + "ms",
+      time_s: time / 1e9 + "s",
+    }));
+    console.log(`Benchmark for ${description}:`);
+    console.table(res);
+    return res;
+  };
+
+  const runner = (index) => {
+    try {
+      fns[index].fn();
+    } catch (err) {
+      console.error(`Error running ${fns[index].name}:
+                ${err}`);
+    }
+  };
+
+  return api;
 };
 
 module.exports = easyBench;
